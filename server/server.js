2d784344a28e4b734be9f2af0ee5e7c98e506aa6
stacknow <stacknow@outlook.com>
2025-04-09 03:31:54 +0000

    see if any

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello from Server!'));
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(6000, () => {
+  console.log('Server running on port 6000');
 });
